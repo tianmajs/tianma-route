99a0f5839341bfc462d777f6dfc9bdd1037425e9
houfeng.hf <houfeng.hf@alibaba-inc.com>
2015-05-10 16:33:16 +0800

    format & ignore

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ var ptr = require('path-to-regexp');
 
 var PATTERN_RULE = /^([a-z]+)?\s*(\/.*)?$/i;
 
-module.exports = function (rule) {
-	var keys = [],
-		re = (rule || '/').match(PATTERN_RULE),
-		method = new RegExp(re[1] || '.*', 'i'),
-		pathname = ptr(re[2], keys);
+module.exports = function(rule) {
+    var keys = [],
+        re = (rule || '/').match(PATTERN_RULE),
+        method = new RegExp(re[1] || '.*', 'i'),
+        pathname = ptr(re[2], keys);
 
-	return function *(next) {
-		var req = this.request,
-			re;
+    return function*(next) {
+        var req = this.request,
+            re;
 
-		if (re = req.method().match(method)) { // Assign.
-			if (re = req.pathname.match(pathname)) { // Assign.
-				req.params = {};
-				
-				keys.forEach(function (key, index) {
-					req.params[key.name] = re[index + 1];
-				});
-				
-				yield next;
-			}
-		}
-	};
+        if (re = req.method().match(method)) { // Assign.
+            if (re = req.pathname.match(pathname)) { // Assign.
+                req.params = {};
+
+                keys.forEach(function(key, index) {
+                    req.params[key.name] = re[index + 1];
+                });
+
+                yield next;
+            }
+        }
+    };
 };
